ff7fa5cc48eee49e52a7544d35f2edcb101870bc
Sergey Knigin <sergey.knigin.dev@gmail.com>
2021-08-27 16:08:19 +0300

    obvious entity saving

---
+++ src/ObjectStorage.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sbooker\TransactionManager;
+
+/**
+ * @internal
+ */
+final class ObjectStorage
+{
+    /** @var array<int, <array<object>>  */
+    private array $store = [];
+
+    public function addAtLevel(int $level, object $object): void
+    {
+        $this->store[$level][spl_object_hash($object)] = $object;
+    }
+
+    public function clear(): void
+    {
+        $this->store = [];
+    }
+
+    public function clearLevelsLowestAndEqualsThan(int $level): void
+    {
+        for($i = $level; $i <= $this->getMaxLevel(); $i++) {
+            unset($this->store[$i]);
+        }
+    }
+
+    private function getMaxLevel(): int
+    {
+        $levels = array_keys($this->store);
+        if (count($levels) > 0) {
+            return max($levels);
+        }
+
+        return 0;
+    }
+
+    public function getFromAllLevels(): array
+    {
+        return
+            array_values(
+                array_reduce(
+                    $this->store,
+                    fn(array $carry, array $objectsAtLevel): array => array_merge($carry, $objectsAtLevel),
+                    []
+                )
+            );
+    }
+}
---
+++ tests/ObjectStorageTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sbooker\TransactionManager\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Sbooker\TransactionManager\ObjectStorage;
+
+/**
+ * @covers \Sbooker\TransactionManager\ObjectStorage
+ */
+final class ObjectStorageTest extends TestCase
+{
+    public function testClearingAtLevel(): void
+    {
+        $storage = new ObjectStorage();
+        $firstObject = (object)['p' => 'first'];
+        $secondObject = (object)['p' => 'second'];
+        $storage->addAtLevel(1, $firstObject);
+        $storage->addAtLevel(2, $secondObject);
+
+        $storage->clearLevelsLowestAndEqualsThan(2);
+
+        $this->assertEquals([$firstObject], $storage->getFromAllLevels());
+    }
+
+    public function testDuplecateAddition(): void
+    {
+        $storage = new ObjectStorage();
+        $object = (object)['p' => 'value'];
+
+        $storage->addAtLevel(1, $object);
+        $storage->addAtLevel(1, $object);
+
+        $this->assertEquals([$object], $storage->getFromAllLevels());
+    }
+}
